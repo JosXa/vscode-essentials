bc7cadfc1ec7327d007e37f2271713a67913d8b6
JosXa <info@josxa.dev>
2020-12-13 03:05:06 +0100

    Initial commit

---
+++ convert.py
@@ -0,0 +1,21 @@
+"""
+Strips comments from the configuration.json file and writes the result into package.json
+"""
+import json
+import re
+from pathlib import Path
+
+pkgjson_file = (Path(__file__).parent / "package.json")
+
+package_json = json.loads(pkgjson_file.read_text())
+
+
+config_json_content = (Path(__file__).parent / "configuration.json").read_text()
+sanitized = re.sub(r'\/\/.*$', '', config_json_content, flags=re.MULTILINE)
+config_json = json.loads(sanitized)
+
+package_json["extensionPack"] = config_json["extensionPack"]
+pkgjson_file.write_text(json.dumps(package_json, indent=4))
+
+
+
